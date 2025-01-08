398948c9df8f2abece1d7980e98bb697264c48f0
Art4v <aarav_20@outlook.com>
2025-01-08 14:44:29 +1100

    main.py returns everything in ics file as a json

--- 99.95/backend/main.py
+++ 99.95/backend/main.py
@@ -1,17 +1,18 @@
 # import libraries
 import datetime 
+from datetime import timedelta
 import icalendar
 import json
 import os 
 import pytz
 
+# get timezone for sydney
+sydney_timezone = pytz.timezone('Australia/Sydney')
+
 # create function to extract dates relavent to today from given ics file
-def events_today(ics_file):
+def events_today(ics_file, date):
   # read ics file
   cal = icalendar.Calendar.from_ical(open(ics_file, 'rb').read())
-
-  # get today's date
-  today = datetime.date(2024, 8, 22)
 
   # create lists to return
   events_today = []
@@ -20,9 +21,6 @@ def events_today(ics_file):
   events_period = []
   events_start = []
   events_end = []
-  
-  # get timezone for sydney
-  sydney_timezone = pytz.timezone('Australia/Sydney')
 
   # go through entire dataset
   for component in cal.walk():
@@ -34,7 +32,7 @@ def events_today(ics_file):
         start_date = start_date.replace(tzinfo=pytz.utc)
         sydney_start = start_date.astimezone(sydney_timezone)
 
-        if sydney_start.date() == today:
+        if sydney_start.date() == date:
             # get end date and time
             end_date = component.get('dtend').dt
             
@@ -54,19 +52,11 @@ def events_today(ics_file):
 
   return events_today, events_location, events_teacher, events_period, events_start, events_end
 
-# inputs ics path and extract relavent data
+# inputs ics and json file path
 ics_file_path = os.path.join('99.95','backend', 'Aarav.ics')
-todays_events, todays_locations, todays_teacher, todays_period, todays_start, todays_end = events_today(ics_file_path)
-
-# print relavent data
-for i in range(len(todays_events)):
-   print('\n', todays_events[i], 'in', todays_locations[i], 'with', todays_teacher[i], todays_period[i], 'from', todays_start[i], 'to', todays_end[i])
-
-
-# ***
-
-# create json file
 json_path = "99.95/backend/output.json"
+
+# open json file for writing
 with open(json_path , "w") as f:
    json.dump([], f, indent=4)
 
@@ -74,11 +64,33 @@ with open(json_path , "w") as f:
 with open (json_path) as f:
    data = json.load(f)
 
-# append variable list
-for i in range(len(todays_events)):
-   temp = {"Class": todays_events[i], "Location": todays_locations[i], "Teacher": todays_teacher[i], "Period": todays_period[i] ,"Start Time": todays_start[i], "End Time": todays_end[i]}
-   data.append(temp)
+# appends all dates in ical to ical_dates_temp
+cal = icalendar.Calendar.from_ical(open(ics_file_path, 'rb').read())
+ical_dates = []
+for component in cal.walk():
+   if component.name == "VEVENT":
+      tempdate = component.get('dtstart').dt
+      tempdate = tempdate.replace(tzinfo=pytz.utc)
+      tempdatesyd = tempdate.astimezone(sydney_timezone)
+
+      ical_dates.append(tempdatesyd.date())
+
+# find no. of days between first and last event
+d0 = ical_dates[0]
+d1 = ical_dates[len(ical_dates) - 1]
+delta = d1 - d0
+
+# parse function through all dates in ics file
+for i in range(delta.days + 1):
+   parsed_date = ical_dates[0] + timedelta(days=i)
+   todays_events, todays_locations, todays_teacher, todays_period, todays_start, todays_end = events_today(ics_file_path, parsed_date)
+   for j in range(len(todays_events)):
+      print(todays_events[j], 'in', todays_locations[j], 'with', todays_teacher[j], todays_period[j], 'from', todays_start[j], 'to', todays_end[j])
+      temp = {"Class": todays_events[j], "Location": todays_locations[j], "Teacher": todays_teacher[j], "Period": todays_period[j] ,"Start Time": todays_start[j], "End Time": todays_end[j]}
+      data.append(temp)
+   print("\n***\n")
 
 # copy variable list back to json file
 with open(json_path, 'w') as f:
    json.dump(data, f, indent=4)
+
