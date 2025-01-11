97ceff9f73df32121a59e6259de2bb5d9fe5172d
Art4v <aarav_20@outlook.com>
2025-01-11 21:52:30 +1100

    Made new python script converting ical to json
    holy shit this took so long im so happy it works

--- 99.95/backend/main.py
+++ 99.95/backend/main.py
@@ -1,5 +1,4 @@
 # import libraries
-import datetime 
 from datetime import timedelta
 import icalendar
 import json
@@ -9,88 +8,87 @@ import pytz
 # get timezone for sydney
 sydney_timezone = pytz.timezone('Australia/Sydney')
 
-# create function to extract dates relavent to today from given ics file
-def events_today(ics_file, date):
-  # read ics file
-  cal = icalendar.Calendar.from_ical(open(ics_file, 'rb').read())
-
-  # create lists to return
-  events_today = []
-  events_location = []
-  events_teacher = []
-  events_period = []
-  events_start = []
-  events_end = []
-
-  # go through entire dataset
-  for component in cal.walk():
-    if component.name == "VEVENT":
-        # get start date and time
-        start_date = component.get('dtstart').dt
-
-        # convert start date and time to sydney time
-        start_date = start_date.replace(tzinfo=pytz.utc)
-        sydney_start = start_date.astimezone(sydney_timezone)
-
-        if sydney_start.date() == date:
-            # get end date and time
-            end_date = component.get('dtend').dt
-            
-            # convert end date and time to sydney time
-            end_date = end_date.replace(tzinfo=pytz.utc)
-            sydney_end = end_date.astimezone(sydney_timezone)
-
-            # append to list as STRINGS
-            events_today.append(component.get('summary'))
-            events_location.append(component.get('location'))
-            events_start.append(sydney_start.strftime("%Y-%m-%d %H:%M:%S"))
-            events_end.append(sydney_end.strftime("%Y-%m-%d %H:%M:%S"))
-
-            temp = component.get('description')
-            events_teacher.append(temp.splitlines()[0])
-            events_period.append(temp.splitlines()[1])
-
-  return events_today, events_location, events_teacher, events_period, events_start, events_end
-
-# inputs ics and json file path
+# get ics file path and open for reading
 ics_file_path = os.path.join('99.95','backend', 'Aarav.ics')
-json_path = "99.95/backend/output.json"
-
-# open json file for writing
-with open(json_path , "w") as f:
-   json.dump([], f, indent=4)
-
-# copy json file as a variable list
-with open (json_path) as f:
-   data = json.load(f)
-
-# appends all dates in ical to ical_dates_temp
 cal = icalendar.Calendar.from_ical(open(ics_file_path, 'rb').read())
-ical_dates = []
-for component in cal.walk():
-   if component.name == "VEVENT":
-      tempdate = component.get('dtstart').dt
-      tempdate = tempdate.replace(tzinfo=pytz.utc)
-      tempdatesyd = tempdate.astimezone(sydney_timezone)
 
-      ical_dates.append(tempdatesyd.date())
+# get the directory of the current Python script
+script_dir = os.path.dirname(__file__)
 
-# find no. of days between first and last event
-d0 = ical_dates[0]
-d1 = ical_dates[len(ical_dates) - 1]
-delta = d1 - d0
+# Define the path for the JSON file in the same directory
+json_file = os.path.join(script_dir, "output.json")
 
-# parse function through all dates in ics file
-for i in range(delta.days + 1):
-   parsed_date = ical_dates[0] + timedelta(days=i)
-   todays_events, todays_locations, todays_teacher, todays_period, todays_start, todays_end = events_today(ics_file_path, parsed_date)
-   for j in range(len(todays_events)):
-      print(todays_events[j], 'in', todays_locations[j], 'with', todays_teacher[j], todays_period[j], 'from', todays_start[j], 'to', todays_end[j])
-      temp = {"Class": todays_events[j], "Location": todays_locations[j], "Teacher": todays_teacher[j], "Period": todays_period[j] ,"Start Time": todays_start[j], "End Time": todays_end[j]}
-      data.append(temp)
-   print("\n***\n")
+# create master dictionary
+master_dict = {}
 
-# copy variable list back to json file
-with open(json_path, 'w') as f:
-   json.dump(data, f, indent=4)
+# create a function to read data from ics file and store immediately in master_dict
+def ical_to_json(date):    
+    # create an array (list) in the master dictionary named after the date
+    master_dict[str(date.date())] = []
+
+    for component in cal.walk():
+        if component.name == "VEVENT":
+            # get the start date of event
+            start_date = component.get('dtstart').dt
+
+            # convert start date to Sydney Time
+            start_date = start_date.replace(tzinfo=pytz.utc)
+            sydney_start = start_date.astimezone(sydney_timezone)
+
+            # check if event date is today's date
+            if sydney_start.date() == date.date():
+                # get end date and convert to sydney time in preperation to append to the array
+                end_date = component.get('dtend').dt
+                end_date = end_date.replace(tzinfo=pytz.utc)
+                sydney_end = end_date.astimezone(sydney_timezone)                
+
+                # append nameless object (dictionary) with relevant data to array
+                master_dict[str(date.date())].append({
+                    "class": str(component.get('summary').partition(": ")[0]),
+                    "name": str(component.get('summary').partition(": ")[2]),
+                    "location": str(component.get('location').partition(": ")[2]),
+                    "teacher": str(component.get('description').splitlines()[0].partition(": ")[2]),
+                    "period": str(component.get('description').splitlines()[1].partition(": ")[2]),
+                    "start_time": str(sydney_start.strftime("%Y-%m-%dT%H:%M:%S")),
+                    "end_time": str(sydney_end.strftime("%Y-%m-%dT%H:%M:%S")),
+
+                })
+
+# create function to determine no. of days ics file spans over, inclusive 
+def days_covered_by(cal):
+    # create array to store all dates in ical, including duplicates
+    all_ical_dates = []
+
+    # go through list an append every date into all_ical_dates
+    for component in cal.walk():
+        if component.name == "VEVENT":
+            tempdate = component.get('dtstart').dt
+            tempdate = tempdate.replace(tzinfo=pytz.utc)
+            tempdatesyd = tempdate.astimezone(sydney_timezone)
+            all_ical_dates.append(tempdatesyd.date())
+
+    # find difference between first and last added date
+    d0 = all_ical_dates[0]
+    d1 = all_ical_dates[len(all_ical_dates) - 1]
+    delta  = d1 - d0
+
+    return delta.days + 1
+
+# create function to determine first date in ical:
+def first_date(cal):
+    for component in cal.walk():
+        if component.name == "VEVENT":
+            tempdate = component.get('dtstart').dt
+            tempdate = tempdate.replace(tzinfo=pytz.utc)
+            first_date = tempdate.astimezone(sydney_timezone)
+            return first_date
+
+# run ical_to_json accross all dates in calenda
+for i in range(days_covered_by(cal)):
+    ical_to_json(first_date(cal) + timedelta(days = i))
+
+
+# dump master_dict to json
+with open(json_file, "w") as f:
+    json.dump(master_dict, f, indent=4)
 
